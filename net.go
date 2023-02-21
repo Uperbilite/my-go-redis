00efe7375ac6908622baee4006c8873cd441fcfa
uperbilite <uperbilite@gmail.com>
2023-02-21 15:08:43 +0800

    ADD: bind and listen in net.TcpServer.

--- net.go
+++ net.go
@@ -3,7 +3,24 @@ package main
 import (
 	"golang.org/x/sys/unix"
 	"log"
+	"strconv"
+	"strings"
 )
+
+const BACKLOG = 64
+
+func addrInet4ToBytes(addr string) ([4]byte, error) {
+	var result [4]byte
+	addrs := strings.Split(addr, ".")
+	for i := 0; i < 4; i++ {
+		a, err := strconv.Atoi(addrs[i])
+		if err != nil {
+			return [4]byte{}, err
+		}
+		result[i] = byte(a)
+	}
+	return result, nil
+}
 
 func TcpServer(port int, addr string) (int, error) {
 	s, err := unix.Socket(unix.AF_INET, unix.SOCK_STREAM, 0)
@@ -12,7 +29,25 @@ func TcpServer(port int, addr string) (int, error) {
 		return -1, nil
 	}
 
-	// TODO: bind & listen
+	var sockAddr unix.SockaddrInet4
+	sockAddr.Port = port
+	sockAddr.Addr, err = addrInet4ToBytes(addr)
+	if err != nil {
+		log.Printf("invalid server addr: %v\n", addr)
+		return -1, nil
+	}
+
+	err = unix.Bind(s, &sockAddr)
+	if err != nil {
+		log.Printf("bind addr err: %v\n", err)
+		return -1, nil
+	}
+
+	err = unix.Listen(s, BACKLOG)
+	if err != nil {
+		log.Printf("listen socket err: %v\n", err)
+		return -1, nil
+	}
 
 	return s, nil
 }
