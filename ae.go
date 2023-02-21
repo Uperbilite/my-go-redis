992964ef6a960bcab2e516ffb6c6f0e8cec8d66b
uperbilite <uperbilite@gmail.com>
2023-02-21 15:45:19 +0800

    ADD: ae proc constants.

--- ae.go
+++ ae.go
@@ -1,20 +1,37 @@
 package main
 
-type FileProc func(loop AeLoop, fd int, mask int)
-type TimeProc func(loop AeLoop, id int) int
+type FeType int
+
+const (
+	AE_READABLE FeType = 1
+	AE_WRITABLE FeType = 2
+)
+
+type TeType int
+
+const (
+	AE_NORMAL TeType = 1
+	AE_ONCE   TeType = 1
+)
+
+type FileProc func(loop *AeLoop, fd int, clientData interface{}, mask FeType)
+type TimeProc func(loop *AeLoop, id int, clientData interface{}) int
 
 type AeFileEvent struct {
-	fd   int
-	mask int
-	proc FileProc
-	next *AeFileEvent
+	fd         int
+	mask       FeType
+	proc       FileProc
+	clientData interface{}
+	next       *AeFileEvent
 }
 
 type AeTimeEvent struct {
-	id   int
-	when int64
-	proc TimeProc
-	next *AeTimeEvent
+	id         int
+	mask       TeType
+	when       int64
+	proc       TimeProc
+	clientData interface{}
+	next       *AeTimeEvent
 }
 
 type AeLoop struct {
