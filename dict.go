0762a0a2c5c23b790ff9ba7b39c61d479cf3ea57
uperbilite <uperbilite@gmail.com>
2023-02-27 00:14:13 +0800

    ADD: dict expand and rehash logic.

--- dict.go
+++ dict.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"errors"
+	"math"
+)
+
+const (
+	DICT_HT_INITIAL_SIZE    int64 = 8
+	DICT_FORCE_RESIZE_RATIO int64 = 3
+	DICT_HT_GROW_RATIO      int64 = 2
+)
+
+var (
+	EP_ERR = errors.New("expand error")
+)
+
 type DictEntry struct {
 	key  *RedisObj
 	val  *RedisObj
@@ -9,7 +24,7 @@ type DictEntry struct {
 type DictHashTable struct {
 	table []*DictEntry
 	size  int64
-	mask  int64
+	mask  int64 // size mask
 	used  int64
 }
 
@@ -20,7 +35,7 @@ type DictType struct {
 
 type Dict struct {
 	DictType
-	HashTable [2]DictHashTable
+	HashTable [2]*DictHashTable
 	rehashidx int64
 }
 
@@ -34,16 +49,84 @@ func (dict *Dict) DictIsRehashing() bool {
 	return dict.rehashidx != -1
 }
 
+func (dict *Dict) DictRehash(step int) {
+	for step > 0 {
+		// exchange hash table if rehash is completed.
+		if dict.HashTable[0].used == 0 {
+			dict.HashTable[0] = dict.HashTable[1]
+			dict.HashTable[1] = nil
+			dict.rehashidx = -1
+			return
+		}
+
+		// find a not null head entry.
+		for dict.HashTable[0].table[dict.rehashidx] == nil {
+			dict.rehashidx += 1
+		}
+
+		// hash all the entry behind head entry, including head entry.
+		var de, nextDe *DictEntry
+		de = dict.HashTable[0].table[dict.rehashidx]
+		for de != nil {
+			nextDe = de.next
+			h := dict.HashFunction(de.key) & dict.HashTable[1].mask
+			de.next = dict.HashTable[1].table[h]
+			dict.HashTable[1].table[h] = de
+			dict.HashTable[1].used += 1
+			dict.HashTable[0].used -= 1
+			de = nextDe
+		}
+
+		dict.HashTable[0].table[dict.rehashidx] = nil
+		dict.rehashidx += 1
+		step -= 1
+	}
+}
+
 func dictNextPower(size int64) int64 {
-	return 0
+	for i := DICT_HT_INITIAL_SIZE; i < math.MaxInt64; i *= 2 {
+		if i >= size {
+			return i
+		}
+	}
+	return -1
 }
 
-func (dict *Dict) dictExpandIfNeeded() {
+func (dict *Dict) DictExpand(size int64) error {
+	realSize := dictNextPower(size)
+	if dict.DictIsRehashing() || dict.HashTable[0].used > size {
+		return EP_ERR
+	}
 
+	var n DictHashTable
+	n.size = realSize
+	n.mask = realSize - 1
+	n.table = make([]*DictEntry, realSize)
+	n.used = 0
+
+	// the first initialization.
+	if dict.HashTable[0] == nil {
+		dict.HashTable[0] = &n
+		return nil
+	}
+
+	// expanded hash table.
+	dict.HashTable[1] = &n
+	dict.rehashidx = 0
+	return nil
 }
 
-func (dict *Dict) DictExpand(size int64) {
-
+func (dict *Dict) dictExpandIfNeeded() error {
+	if dict.DictIsRehashing() {
+		return nil
+	}
+	if dict.HashTable[0].size == 0 {
+		return dict.DictExpand(DICT_HT_INITIAL_SIZE)
+	}
+	if (dict.HashTable[0].used > dict.HashTable[0].size) && (dict.HashTable[0].used/dict.HashTable[0].size > DICT_FORCE_RESIZE_RATIO) {
+		return dict.DictExpand(dict.HashTable[0].size * DICT_HT_GROW_RATIO)
+	}
+	return nil
 }
 
 func (dict *Dict) DictGetRandomKey() (key, val *RedisObj) {
