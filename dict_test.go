06a5b2f61decb1b2ecad918a2b31cf3e410b3465
uperbilite <uperbilite@gmail.com>
2023-02-27 09:33:42 +0800

    ADD: rehashing testcase.

--- dict_test.go
+++ dict_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -20,6 +21,8 @@ func TestDict(t *testing.T) {
 	assert.Nil(t, err)
 	k1.IncrRefCount()
 	v1.IncrRefCount()
+	assert.Equal(t, 2, k1.refcount)
+	assert.Equal(t, 2, v1.refcount)
 
 	entry = d.DictFind(k1)
 	assert.Equal(t, k1, entry.Key)
@@ -29,4 +32,48 @@ func TestDict(t *testing.T) {
 	assert.Nil(t, err)
 	entry = d.DictFind(k1)
 	assert.Nil(t, entry)
+	assert.Equal(t, 1, k1.refcount)
+	assert.Equal(t, 1, v1.refcount)
+}
+
+func TestRehash(t *testing.T) {
+	d := DictCreate(DictType{
+		HashFunction: RedisStrHash,
+		KeyCompare:   RedisStrEqual,
+	})
+	entry := d.DictGetRandomKey()
+	assert.Nil(t, entry)
+
+	size := int(DICT_HT_INITIAL_SIZE * (DICT_FORCE_RESIZE_RATIO + 1))
+	for i := 0; i < size; i++ {
+		key := CreateObject(REDISSTR, fmt.Sprintf("k%v", i))
+		val := CreateObject(REDISSTR, fmt.Sprintf("v%v", i))
+		err := d.DictAdd(key, val)
+		assert.Nil(t, err)
+	}
+	assert.Equal(t, false, d.DictIsRehashing())
+
+	key := CreateObject(REDISSTR, fmt.Sprintf("k%v", size))
+	val := CreateObject(REDISSTR, fmt.Sprintf("v%v", size))
+	err := d.DictAdd(key, val)
+	assert.Nil(t, err)
+	assert.Equal(t, true, d.DictIsRehashing())
+	assert.Equal(t, int64(0), d.rehashidx)
+	assert.Equal(t, DICT_HT_INITIAL_SIZE, d.HashTable[0].size)
+	assert.Equal(t, DICT_HT_INITIAL_SIZE*DICT_HT_GROW_RATIO, d.HashTable[1].size)
+
+	for i := 0; i < int(d.HashTable[0].size)+1; i++ {
+		d.DictGetRandomKey()
+	}
+	assert.Equal(t, false, d.DictIsRehashing())
+	assert.Equal(t, DICT_HT_INITIAL_SIZE*DICT_HT_GROW_RATIO, d.HashTable[0].size)
+	assert.Nil(t, d.HashTable[1])
+
+	for i := 0; i < size+1; i++ {
+		key := CreateObject(REDISSTR, fmt.Sprintf("k%v", i))
+		entry := d.DictFind(key)
+		assert.NotNil(t, entry)
+		assert.Equal(t, fmt.Sprintf("v%v", i), entry.Val.StrVal())
+	}
+
 }
